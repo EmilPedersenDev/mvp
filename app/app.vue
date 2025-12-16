77cbde5a22b98feac54fa50138ac27165fd9a175
RefinedWiki Build Agent <bamboo@refinedwiki.com>
2025-12-16 12:48:44 +0100

    adding supabase connection

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1>Mvp</h1>
+    <img v-if="imageUrl" :src="imageUrl" alt="wunderbaum">
   </div>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient()
+
+const { data } = await useAsyncData('wunderbaum-image', async () => {
+  const { data } = await supabase
+    .storage
+    .from('files')
+    .getPublicUrl('wunderbaum.png')
+  return data.publicUrl
+})
+
+const imageUrl = data.value
+</script>
